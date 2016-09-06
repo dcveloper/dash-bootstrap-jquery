cdb08a1b8ad3681dd509fe0ab842378476d12a52
Jonathan Webb <jonathan.webb@treasury.gov>
2016-09-06 14:47:30 -0400

    Dashboard cards ui SCSS and template files.  Some changes to KPI cards ui as well.

---
+++ src/app/dashboardcard/dashboardcard.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'dashboardcard',  // <dashboardcard></dashboardcard>
+  providers: [
+
+  ],
+  directives: [
+
+  ],
+  pipes: [ ],
+  styleUrls: [ './dashboardcard.style.css' ],
+  templateUrl: './dashboardcard.template.html'
+})
+export class Dashboardcard {
+
+
+  // TypeScript public modifiers
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    console.log('hello `dashboardcard` component');
+
+  }
+
+  submitState(value) {
+
+  }
+
+}
---
+++ src/app/kpicard/index.ts
@@ -0,0 +1,1 @@
+export * from './kpicard.component';
